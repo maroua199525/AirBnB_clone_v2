b0f4ac8e57971d39d4bbc19ce16be4bb1b34f553
RimJoudi <joudirim93@gmail.com>
2021-08-11 21:37:34 +0100

    commit to amenty file

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+import sqlalchemy
+from sqlalchemy import Column, String
+from os import getenv
+from sqlalchemy.orm import relationship
+from models.place import place_amenity
 
+class Amenity(BaseModel, Base):
+    '''
+        Amenity class
+    '''
+    __tablename__ = 'amenities'
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary=place_amenity, back_populates="amenities")
 
-class Amenity(BaseModel):
-    name = ""
+    else:
+        name = ""
