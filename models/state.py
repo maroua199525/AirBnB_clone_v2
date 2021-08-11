42fc03678bad3d308c32536730840dcfe6008fb9
RimJoudi <77971241+RimJoudi@users.noreply.github.com>
2021-08-11 22:14:20 +0100

    commi to the state file

--- models/state.py
+++ models/state.py
@@ -1,8 +1,31 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+import models
+import sqlalchemy
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from os import getenv
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    '''
+        State class
+    '''
+    __tablename__ = 'states'
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", cascade="all, delete-orphan")
+
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            instance_list = []
+            for key, obj in models.storage.all().items():
+                if obj.__class__.__name__ == 'City':
+                    if obj.state_id == self.id:
+                        instance_list.append(obj)
+            return instance_list
