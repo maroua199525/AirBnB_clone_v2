b9f0585660d87633ed6b1725fbb4d9315a0933ca
maroua199525 <2646@holbertonschool.com>
2021-08-28 21:34:42 +0100

    add change owner

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,13 +2,10 @@
 # a Bash script that sets up your web servers for the deployment of web_static
 sudo apt-get update -y
 sudo apt-get install -y nginx
-sudo chown -R ubuntu:ubuntu /data/
-mkdir -p /data/
-mkdir -p /data/web_static/
-mkdir -p /data/web_static/releases/
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
-sudo echo "Holberton School" > /data/web_static/releases/test/index.html
+sudo echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 ln -s -f /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
