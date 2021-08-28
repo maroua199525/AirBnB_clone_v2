f78a8f8e893227dc593e13d3ba0b493867bb0b5e
maroua199525 <2646@holbertonschool.com>
2021-08-28 21:30:58 +0100

    sets up your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,23 +3,12 @@
 sudo apt-get update -y
 sudo apt-get install -y nginx
 sudo chown -R ubuntu:ubuntu /data/
-sudo chmod -R 755 /var/www
+mkdir -p /data/
+mkdir -p /data/web_static/
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
 sudo echo "Holberton School" > /data/web_static/releases/test/index.html
-if (!file_exists('/data/')) {
-    mkdir('/data/', 0777, true);
-}
-if (!file_exists('/data/web_static/')) {
-    mkdir('/data/web_static/', 0777, true);
-}
-if (!file_exists('/data/web_static/releases/')) {
-    mkdir('/data/web_static/releases/', 0777, true);
-}
-if (!file_exists('/data/web_static/shared/')) {
-    mkdir('/data/web_static/shared/', 0777, true);
-}
-if (!file_exists('/data/web_static/releases/test/')) {
-    mkdir('/data/web_static/releases/test/', 0777, true);
-}
 ln -s -f /data/web_static/releases/test/ /data/web_static/current
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
