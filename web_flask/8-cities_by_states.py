28f4ff1da67fe22d9c950ef22fb9662c0377f176
maroua199525 <2646@holbertonschool.com>
2021-09-11 20:48:08 +0100

    add tasks

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Module Flask"""
+
+from flask import Flask, escape, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+@app.teardown_appcontext
+def teardown_data(self):
+    """
+        remove the current SQLAlchemy Session:
+    """
+    storage.close()
+
+@app.route('/cities_by_states', strict_slashes=False)
+def hello_world():
+    """ return all states in the db  """
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
